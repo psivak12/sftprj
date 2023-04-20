f1b7d38f84b9a98c53a3debcd23a9ab284178f5d
psivak12 <psivak12@my.centennialcollege.ca>
2023-04-20 11:50:51 -0400

    review analysis

---
+++ app.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Sentiment Analysis
+"""
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+from textblob import TextBlob
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://bitdigitsurveys:bitdigitsurveys@cluster0.dzcak.mongodb.net/")
+db = client["test"]
+collection = db["surveysResponses"]
+
+
+@app.route('/data')
+def get_data():
+    data = list(collection.find())
+    for item in data:
+        item['_id'] = str(item['_id'])  # Convert ObjectId to string
+    return jsonify(data)
+
+
+@app.route('/add_sentiment')
+def add_sentiment():
+    for doc in collection.find():
+        review = doc['Response']
+        polarity = TextBlob(review).sentiment.polarity
+        collection.update_one(
+            {'_id': doc['_id']},
+            {'$set': {'sentiment_polarity': polarity}}
+        )
+    return 'Sentiment added to all records.'
+
+'''
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
+
